fb79a5c89ab201c693e02c9ad7e9f56673a6bc7d
jhott <ottjh@uc.mail.edu>
2024-12-08 11:10:54 -0500

    Now with file not found return.

--- src/FileScan.java
+++ src/FileScan.java
@@ -1,15 +1,115 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class FileScan
+{
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //copied a lot of code from the provided sample project
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+        ArrayList<String> myWordList = new ArrayList<>(); //Tried to make an array list work so it could read text documents of variable size, but its not worth it anymore.
+        if (args.length > 0)
+        {
+            try
+            {
+                selectedFile = new File(args[0]);
+                if (!selectedFile.exists())
+                {
+                    System.out.println("File not found");
+                    return;
+                }
+                Path file = selectedFile.toPath();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+
+                int wordNum = 0;
+                int charNum = 0;
+                int line = 0;
+                String[] wordsPresent = new String[200];
+                while (reader.ready()) {
+                    rec = reader.readLine();
+                    line++;
+                    wordsPresent = rec.split("\\s+"); //The suggestion in the video of "\\w" didn't work for me. Had to get help.
+                    //myWordList = rec.split("\\s+");
+                    wordNum += wordsPresent.length;
+                    //wordNum += myWordList.size();
+                    charNum += rec.length();
+
+
+                    System.out.printf("\nLine %4d %-60s ", line, rec);
+                }
+            }
+            catch (FileNotFoundException e) {
+                System.out.println("File not found!!!");
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            try
+            {
+
+                //Had no idea how to use the get property command correctly.
+                File workingDirectory = new File("C:\\Users\\jhott\\IdeaProjects\\Lab_14_CMD_Scripting\\src");
+
+
+                chooser.setCurrentDirectory(workingDirectory);
+
+
+                if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    selectedFile = chooser.getSelectedFile();
+                    Path file = selectedFile.toPath();
+
+                    InputStream in =
+                            new BufferedInputStream(Files.newInputStream(file, CREATE));
+                    BufferedReader reader =
+                            new BufferedReader(new InputStreamReader(in));
+
+                    int wordNum = 0;
+                    int charNum = 0;
+                    int line = 0;
+                    String[] wordsPresent = new String[200];
+                    while (reader.ready()) {
+                        rec = reader.readLine();
+                        line++;
+                        wordsPresent = rec.split("\\s+"); //The suggestion in the video of "\\w" didn't work for me. Had to get help.
+                        //myWordList = rec.split("\\s+");
+                        wordNum += wordsPresent.length;
+                        //wordNum += myWordList.size();
+                        charNum += rec.length();
+
+
+                        System.out.printf("\nLine %4d %-60s ", line, rec);
+                    }
+                    reader.close();
+                    System.out.println("\n\nData file read! File: " + selectedFile.getName());
+                    System.out.println("The Line Count was " + line + ". The number of words read was " + wordNum + ". The number of characters read was " + charNum + ".");
+                    //for (int i = 0; i < wordsPresent.length; i++) {
+                    //  System.out.println(wordsPresent[i]); //Tested to see what words were in the array when the split wasn't working.
+                    //}
+
+                } else {
+                    System.out.println("No file selected!!! ... exiting.\nRun the program again and select a file.");
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found!!!");
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,4 +1,201 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class SafeInput {
+public class SafeInput
+{
+    /**
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a String response that is not zero length
+     */
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retString = ""; // Set this to zero length. Loop runs until it isn't
+        do
+        {
+            System.out.print("\n" +prompt + ": "); // show prompt add space
+            retString = pipe.nextLine();
+        }while(retString.length() == 0);
+
+        return retString;
+
+    }
+    public static int getInt(Scanner pipe, String prompt)
+    {
+        //declaring and initializing
+        int intRetValue = 0;
+        String trash = "";
+        boolean done = false;
+
+        //Loop test for valid integer
+        do {
+            System.out.print("\n" + prompt + ": ");
+            if (pipe.hasNextInt()) {
+                intRetValue = pipe.nextInt();
+                pipe.nextLine();
+                done = true;
+            } else {
+                trash = pipe.nextLine();
+                System.out.println("You must enter a valid whole number not: " + trash);
+            }
+        }while (!done);
+
+        //send back value
+        return intRetValue;
+    }
+    public static double getDouble(Scanner pipe, String prompt)
+    {
+        //declaring and initializing
+        double doubleRetValue = 0;
+        String trash = "";
+        boolean done = false;
+
+        //Loop test for valid double
+        do {
+            System.out.print("\n" + prompt + ": ");
+            if (pipe.hasNextDouble()) {
+                doubleRetValue = pipe.nextDouble();
+                pipe.nextLine();
+                done = true;
+            } else {
+                trash = pipe.nextLine();
+                System.out.println("You must enter a valid number not: " + trash);
+            }
+        }while (!done);
+
+        //send back value
+        return doubleRetValue;
+    }
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high) //I can just use the int one here and nest an additional requirement inside
+    {
+        //declaring and initializing
+        int intRangedRetValue = 0;
+        String trash = "";
+        boolean done = false;
+
+        //Loop test for valid integer
+        do {
+            System.out.print("\n" + prompt + ": ");
+            if (pipe.hasNextInt())
+            {
+                intRangedRetValue = pipe.nextInt();
+                pipe.nextLine();
+                if (intRangedRetValue >= low && intRangedRetValue <= high)
+                {
+                    done = true;
+                }
+                else
+                {
+                    System.out.println("\nThe number you listed is not in the available range.");
+                }
+            } else {
+                trash = pipe.nextLine();
+                System.out.println("\nYou must enter a valid whole number between " + low + " and " + high + " not: " + trash);
+            }
+        }while (!done);
+
+        //send back value
+        return intRangedRetValue;
+    }
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high) {
+        //declaring and initializing
+        double doubRangedRetValue = 0;
+        String trash = "";
+        boolean done = false;
+
+        //Loop test for valid integer
+        do {
+            System.out.print("\n" + prompt + ": ");
+            if (pipe.hasNextDouble()) {
+                doubRangedRetValue = pipe.nextDouble();
+                pipe.nextLine();
+                if (doubRangedRetValue > low && doubRangedRetValue < high) {
+                    done = true;
+                } else {
+                    System.out.println("\nThe number you listed is not in the available range.");
+                }
+            } else {
+                trash = pipe.nextLine();
+                System.out.println("\nYou must enter a valid number between " + low + " and " + high + " not: " + trash);
+            }
+        } while (!done);
+
+        //send back value
+        return doubRangedRetValue;
+    }
+    public static boolean getYNConfirm(Scanner pipe, String prompt)
+    {
+        //declaring and initializing
+        boolean yNRetValue = false;
+        String yesNo = "";
+        String trash = "";
+        boolean done = false;
+
+        //Loop test for valid integer
+        do {
+            System.out.print("\n" + prompt + ": ");
+            yesNo = pipe.nextLine();
+            if (yesNo.equalsIgnoreCase("y") || yesNo.equalsIgnoreCase("n"))
+            {
+                if (yesNo.equalsIgnoreCase("y"))
+                    yNRetValue = true;
+                else
+                    yNRetValue = false;
+                done = true;
+            }
+            else
+            {
+                System.out.println("You must enter Yes or No [Y/N] not: " + yesNo);
+            }
+        }while (!done);
+        //send back value
+        return yNRetValue;
+    }
+    public static String getRegExString(Scanner pipe, String prompt, String regEx)
+    {
+        boolean done = false;
+        String regString = "";
+
+        do
+        {
+            System.out.print(prompt + ": ");
+            regString = pipe.nextLine();
+            if(regString.matches(regEx))
+                done= true;
+            else
+            {
+                System.out.println("Please enter a valid input, not: " + regString);
+            }
+        } while (!done);
+        return regString;
+    }
+    public static void prettyHeader(String msg)
+    {
+        for(int i = 0; i < 60; i++)
+        {
+            System.out.print("*");
+        }
+        System.out.print("\n***");
+        for(int i = 0; i < (54/2 - msg.length()/2); i++)
+            System.out.print(" ");
+        System.out.print(msg);
+        for(int i = 0; i < (54/2 - msg.length()/2); i++)
+            System.out.print(" ");
+        System.out.print("***\n");
+        for(int i = 0; i < 60; i++)
+        {
+            System.out.print("*");
+        }
+    }
+    public static double getAverage(int values[])
+    {
+        int sum = 0;
+        double average = 0;
+        for (int i = 0; i < values.length; i++)
+        {
+            sum += values[i];
+        }
+        average = ((double) sum / values.length);
+        return average;
+    }
 }
